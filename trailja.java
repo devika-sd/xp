57fd45b9b68976d63a90c9b81a869562d082f551
devika-sd <devikajeevaraj1310@gmail.com>
2018-09-02 19:54:32 +0530

    testy

---
+++ trailja.java
@@ -0,0 +1,18 @@
+public class trailja
+{
+ 	int data;
+	double in;
+	public static void display(int date)
+	{
+	System.out.println(date);
+	}
+}
+class trail extends trailja
+{
+static int date;
+public static void main(String args[])
+{
+	double bit;
+	display(date);
+} 
+}
