dd43f4c3744e70436b426b6a74f692709982442c
devika-sd <devikajeevaraj1310@gmail.com>
2018-09-03 22:08:17 +0530

    mybest

--- mypro/projectpackmain.java
+++ mypro/projectpackmain.java
@@ -24,88 +24,88 @@ case 1:
 	{
 	case 1:
 		gpa1=bio.sem1();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa1);
+		System.out.println("The current GPA is"+gpa1);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa1);
+			p.cgpa(gpa1,n);
 		}
 		break;
 	case 2:
 		gpa2=bio.sem2();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa2);
+		System.out.println("The current GPA is"+gpa2);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa2);
+			p.cgpa(gpa2,n);
 		}
 		break;
 	case 3:
 		gpa3=bio.sem3();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa3);
+		System.out.println("The current GPA is"+gpa3);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa3);
+			p.cgpa(gpa3,n);
 		}
 		break;
 	case 4:
 		gpa4=bio.sem4();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa4);
+		System.out.println("The current GPA is"+gpa4);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa4);
+			p.cgpa(gpa4,n);
 		}
 		break;
 	case 5:
 		gpa5=bio.sem5();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa5);
+		System.out.println("The current GPA is"+gpa5);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa5);
+			p.cgpa(gpa5,n);
 		}
 		break;
 	case 6:
 		gpa6=bio.sem6();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa6);
+		System.out.println("The current GPA is"+gpa6);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa6);
+			p.cgpa(gpa6,n);
 		}
 		break;
 	case 7:
 		gpa7=bio.sem7();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa7);
+		System.out.println("The current GPA is"+gpa7);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa7);
+			p.cgpa(gpa7,n);
 		}
 		break;
 	case 8:
 		gpa8=bio.sem8();
-		System.out.println("if you want to calculate cgpa");
+		System.out.println("do you want to calculate cgpa");
 		y=s.next();
-		System.out.println(gpa8);
+		System.out.println("The current GPA is"+gpa8);
 		if(y.equals("yes"))
 		{
-			p.cgpa(gpa8);
+			p.cgpa(gpa8,n);
 		}
 		break;
 	}
 }
 }
-public void cgpa(double gpa)
+public void cgpa(double gpa,int t)
 {
 biotech bio=new biotech();
 Scanner sc=new Scanner(System.in);
@@ -117,34 +117,32 @@ no=sc.next();
 		System.out.println("enter the previous cgpa");
 		prev=sc.nextDouble();
 		cgpa=(gpa+prev)/2;
-		System.out.println(cgpa);
+		System.out.println("CGPA is"+cgpa);
 	}
 	else
 	{
-		System.out.println("1.1st sem cgpa"+"\n"+"2.2nd sem cgpa"+"\n"+"3.3rd sem cgpa"+"\n"+"4.4th sem cgpa"+"\n"+"5.5st sem cgpa"+"\n"+"6.6th sem cgpa"+"\n"+"7.7th sem cgpa"+"\n"+"8.8th sem cgpa"+"\n");
-	int t=sc.nextInt();
 	switch(t)
 	{
 	case 1: cal();
-		System.out.println(ini);
+		System.out.println("CGPA is"+ini);
 		break;
 	case 2: 
 		gpa1=bio.sem1();
 		cal();
-		System.out.println(frst2);
+		System.out.println("CGPA is"+frst2);
 		break;
 	case 3: 
 		gpa1=bio.sem1();
 		gpa2=bio.sem2();
 		cal();
-		System.out.println(secd1);
+		System.out.println("CGPA is"+secd1);
 		break;
 	case 4: 
 		gpa1=bio.sem1();
 		gpa2=bio.sem2();
 		gpa3=bio.sem3();
 		cal();
-		System.out.println(secd2);
+		System.out.println("CGPA is"+secd2);
 		break;
 	case 5: 
 		gpa1=bio.sem1();
@@ -152,7 +150,7 @@ no=sc.next();
 		gpa3=bio.sem3();
 		gpa4=bio.sem4();
 		cal();
-		System.out.println(trid1);
+		System.out.println("CGPA is"+trid1);
 		break;
 	case 6: 
 		gpa1=bio.sem1();
@@ -161,7 +159,7 @@ no=sc.next();
 		gpa4=bio.sem4();
 		gpa5=bio.sem5();
 		cal();
-		System.out.println(trid2);
+		System.out.println("CGPA is"+trid2);
 		break;
 	case 7: 
 		gpa1=bio.sem1();
@@ -171,7 +169,7 @@ no=sc.next();
 		gpa5=bio.sem5();
 		gpa6=bio.sem6();
 		cal();
-		System.out.println(fin1);
+		System.out.println("CGPA is"+fin1);
 		break;
 	case 8:
 		gpa1=bio.sem1();
@@ -182,7 +180,7 @@ no=sc.next();
 		gpa6=bio.sem6();
 		gpa7=bio.sem7();
 		cal();
-		System.out.println(fin2);
+		System.out.println("CGPA is"+fin2);
 		break;
 	}
 
